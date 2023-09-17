7d4a3b3596df3c37bed94eb60c7213b443e43629
fireguy188 <aryehtys@gmail.com>
2023-09-18 00:14:24 +0100

    Made collisions and map border

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,17 @@
-extends Area2D
-
-var viewport: Viewport
+extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	viewport = get_viewport()
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = viewport.get_mouse_position()
+	pass
+
+func _physics_process(delta):
+	var collision = move_and_collide(get_global_mouse_position() - global_position)
+	
+	if collision:
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
